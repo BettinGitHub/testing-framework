961f7f6c4b6ebb0d8e9f092b8baeabe824bfb9e9
bkj <43181567+HumberCampusNavigator@users.noreply.github.com>
2020-10-03 14:24:53 -0400

    Completed Task 1. Closes #1

---
+++ src/test/java/com/qaguru/demo/LoginTests.java
@@ -0,0 +1,32 @@
+package com.qaguru.demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+    @Test(priority = 1, testName = "Login test with valid credentials")
+    public void loginWithValidUser() {
+        System.out.println("Login test with valid user and password");
+        // Assert.fail("Login Test failed"); //to make a test failed
+        String exp = "Hello";
+        String act = "Hello";
+        Assert.assertEquals(act, exp, "The words don't match");
+    }
+    @Test(priority = 3)
+    public void loginWithInvalidUser(){
+        System.out.println("Login with invalid user");
+    }
+
+    @Test(priority = 2, testName = "Home Page Test")
+    public void homePageTest(){
+        System.out.println("This is a home page test");
+    }
+
+    @Test(priority = 4)
+    public void logoutTest(){
+        System.out.println("This is a logout test");
+    }
+
+
+}
