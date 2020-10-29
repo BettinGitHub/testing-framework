e6e690bef2b7d429c1a961a3a6a578bd3ae7b522
Bettin QA student <bettincash@gmail.com>
2020-10-29 13:34:22 -0400

    Assertions task completed - (Assert.assertEquals, Assert.assertTrue, Assert.fail). Closes #3, #11

--- src/test/java/com/qaguru/demo/LoginTests.java
+++ src/test/java/com/qaguru/demo/LoginTests.java
@@ -16,11 +16,16 @@ public class LoginTests {
     @Test(priority = 3)
     public void loginWithInvalidUser(){
         System.out.println("Login with invalid user");
+        Assert.fail("Invalid user login test failed");
     }
 
     @Test(priority = 2, testName = "Home Page Test")
     public void homePageTest(){
         System.out.println("This is a home page test");
+        String exp = "Homepage";
+        String act = "Homepage";
+        Assert.assertTrue(exp==act, "Home page title is incorrect");
+        Assert.fail("Home page title test failed");
     }
 
     @Test(priority = 4)
