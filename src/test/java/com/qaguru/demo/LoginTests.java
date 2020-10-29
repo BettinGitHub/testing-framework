ef11a536d12722ef1710c7ea8be6f45d3f2177cd
Bettin QA student <bettincash@gmail.com>
2020-10-29 14:23:29 -0400

    Added @BeforeMethod, @AfterMethod, @BeforeClass and @AfterClass annotations. Closes #4

--- src/test/java/com/qaguru/demo/LoginTests.java
+++ src/test/java/com/qaguru/demo/LoginTests.java
@@ -1,9 +1,29 @@
 package com.qaguru.demo;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LoginTests {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("**** Before class executed ****");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("%%%% After Class executed %%%%");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Before Method Executed...");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After method Executed...");
+    }
 
     @Test(priority = 1, testName = "Login test with valid credentials")
     public void loginWithValidUser() {
@@ -13,22 +33,21 @@ public class LoginTests {
         String act = "Hello";
         Assert.assertEquals(act, exp, "The words don't match");
     }
-    @Test(priority = 3)
+    @Test(priority = 3, testName = "Login with an invalid Username and password")
     public void loginWithInvalidUser(){
         System.out.println("Login with a invalid user");
-        Assert.fail("Invalid user login test failed");
+        //Assert.fail("Invalid user login test failed");
     }
 
-    @Test(priority = 2, testName = "Home Page Test")
+    @Test(priority = 2, testName = "Verify Home Page Title")
     public void homePageTest(){
-        System.out.println("This is a home page test");
+        System.out.println("This is a home page title test");
         String exp = "Homepage";
         String act = "Homepage";
         Assert.assertTrue(exp==act, "Home page title is incorrect");
-        Assert.fail("Home page title test failed");
     }
 
-    @Test(priority = 4)
+    @Test(priority = 4, testName="This is a Logout Test")
     public void logoutTest(){
         System.out.println("This is a logout test");
     }
